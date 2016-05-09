58a5ae26c5e4a90611ec58b8a98c8637c0745a40
Darren Bown <dazzlebown@gmail.com>
2016-05-09 12:35:30 +0100

    Safety logout after each scenario

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,8 +1,14 @@
-browser = Watir::Browser.new
+browser = Watir::Browser.new $browser
 
 
 Before do
   @browser = browser
 end
 
-at_exit{browser.close}
+After do
+  @browser.link(title: 'Logout').click if @browser.link(title: 'Logout').present?
+end
+
+at_exit do
+  browser.close
+end
