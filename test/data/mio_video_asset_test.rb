81666f141421a5824a198013c04525c4c8d357f7
Darren Bown <dazzlebown@gmail.com>
2016-05-11 12:46:39 +0100

    Changed MioVideoAssetTest to use factory-generated object

--- test/data/mio_video_asset_test.rb
+++ test/data/mio_video_asset_test.rb
@@ -3,11 +3,11 @@ require_relative '../test_helper'
 require_relative 'mio_video_asset'
 
 class MioVideoAssetTest < Test::Unit::TestCase
+  include MioExceptions
 
   def setup
-    @default_video_asset = MioVideoAsset.new
+    @default_video_asset = FactoryGirl.build :mio_video_asset
   end
-
 
   def test_asset_created
     raise InvalidObjectType unless @default_video_asset.class.equal? MioVideoAsset
@@ -16,6 +16,7 @@ class MioVideoAssetTest < Test::Unit::TestCase
   def test_attributes_present
     #TODO: Complete when mandatory attributes confirmed
     raise AttributeNotFoundException unless @default_video_asset.respond_to? :clip_type
+    raise AttributeNotFoundException unless @default_video_asset.clip_type.length > 0
   end
 
 
