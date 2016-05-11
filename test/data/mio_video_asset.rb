1b579ac90b6c2f8eae780dcae60d00de31e72151
Darren Bown <dazzlebown@gmail.com>
2016-05-11 12:45:18 +0100

    Added 'non-mandatory' trait to Video Asset Factory

--- test/data/mio_video_asset.rb
+++ test/data/mio_video_asset.rb
@@ -2,7 +2,7 @@ class MioVideoAsset
 
   attr_accessor :clip_type, :description, :location, :interviewee, :ft_office, :producer,
                 :freelance_producer, :reporter_writer_1, :reporter_writer_2, :reporter_writer_3,
-                :restrictions, :restriction_description
+                :has_restrictions, :no_restrictions, :restriction_description
 
 
 end
--- test/factories/mio_video_asset_factory.rb
+++ test/factories/mio_video_asset_factory.rb
@@ -2,17 +2,22 @@ require_relative '../../test/data/mio_video_asset'
 
 FactoryGirl.define do
   factory :mio_video_asset, class: MioVideoAsset do |asset|
-    asset.clip_type ''
+    asset.clip_type 'Int'
     asset.description 'AT Test Clip'
-    asset.location ''
-    asset.interviewee 'AT Test Interviewee'
-    asset.ft_office ''
-    asset.producer ''
-    asset.freelance_producer ''
+    asset.location 'Edinburgh, Scotland'
+    asset.ft_office 'One Southwark Bridge'
+    asset.producer 'Steve Ager'
     asset.reporter_writer_1 'AT Reporter Writer 1'
-    asset.reporter_writer_2 'AT Reporter Writer 2'
-    asset.reporter_writer_3 'AT Reporter Writer 3'
-    asset.restrictions 'no'
-    asset.restriction_description 'AT restriction description'
+    asset.no_restrictions 'No'
+
+    trait :non_mandatory do
+      asset.freelance_producer 'Rita Jones'
+      asset.reporter_writer_2 'AT Reporter Writer 2'
+      asset.reporter_writer_3 'AT Reporter Writer 3'
+      asset.interviewee 'Michael Bloomberg'
+      asset.has_restrictions'Yes'
+      asset.restriction_description 'AT restriction description'
+    end
+
   end
 end
