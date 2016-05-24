6d9da5514bf10b43c9912d02134a39b713ec6464
Darren Bown <dazzlebown@gmail.com>
2016-05-24 16:38:26 +0100

    Removed rescue fallback from WS client

--- lib/web_service_clients/mio_webservice_client.rb
+++ lib/web_service_clients/mio_webservice_client.rb
@@ -9,25 +9,12 @@ class MioWebserviceClient
 
   def retrieve_metadata
     puts @url
-    begin
       definition_id = @url.gsub(/[^0-9]/, '')
       VCR.use_cassette("definition_request-#{definition_id}") do
         RestClient::Request.execute(method: :get, url: @url, timeout: 10, user: @username, password: @password, headers: @headers) do |response|
           @response = response
           JSON.parse(response)
       end
-
-    end
-
-
-  rescue RestClient::Exception, JSON::JSONError, SocketError => e
-    $stderr.puts <<ERROR
-                    Cannot connect to service
-                    #{e.class}
-#{p @response}
-                    Using cached field names
-ERROR
-    return nil
     end
   end
 
