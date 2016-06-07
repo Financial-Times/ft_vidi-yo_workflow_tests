e5539830ab00f7f4a01f881a59d15c71894e7ef4
Darren Bown <dazzlebown@gmail.com>
2016-06-07 12:05:04 +0100

    Added bye bug

--- Gemfile
+++ Gemfile
@@ -16,4 +16,5 @@ group :test do
   gem 'rspec'
   gem 'webmock'
   gem 'simplecov'
+  gem 'byebug'
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,7 @@ require 'page-object'
 require 'test-unit'
 require 'yaml'
 require 'factory_girl'
+require 'byebug'
 
 require_relative '../../lib/pages/mio_page'
 require_relative '../../lib/pages/mio_login_page'
@@ -13,6 +14,7 @@ require_relative '../../lib/pages/mio_dashboard_page'
 require_relative '../../config/mio_constants'
 require_relative '../../lib/pages/in_development/ft_login_page'
 require_relative '../../lib/pages/in_development/create_project_page'
+require_relative '../../lib/pages/in_development/ingest_page'
 
 World(PageObject::PageFactory)
 World(FactoryGirl::Syntax::Methods)
