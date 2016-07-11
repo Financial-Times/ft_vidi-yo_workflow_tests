2a97bf19e5bcb28dd01859f41511808f88c7c6ec
Darren Bown <dazzlebown@gmail.com>
2016-07-11 09:23:10 +0100

    Refreshed VCR cassettes

--- vcr_setup.rb
+++ vcr_setup.rb
@@ -7,6 +7,7 @@ LOG_FILE = File.expand_path(File.join(File.dirname(__FILE__), 'logs/vcr_log.log'
 VCR.configure do |c|
   c.cassette_library_dir = File.expand_path(File.join(File.dirname(__FILE__), 'test/vcr_cassettes'))
   c.hook_into :webmock
+ # c.ignore_hosts 'master.dev.nativ-systems.com'
   c.ignore_localhost = true
   c.configure_rspec_metadata!
   c.allow_http_connections_when_no_cassette = true
