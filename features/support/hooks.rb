b04e21593e2cac4e675c0cca47580e43b7ad24a7
Darren Bown <dazzlebown@gmail.com>
2016-07-11 09:23:43 +0100

    Changed Hooks.rb for one-browser-per-scenario

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,15 +2,16 @@
 ENV['SESSION_BROWSER'] ||= 'chrome'
 Selenium::WebDriver::Chrome::Service.executable_path = '/Users/Dazzla/src/chromedriver'
 # driver = Selenium::WebDriver.for :firefox, marionette: true
-browser = Watir::Browser.new ENV['SESSION_BROWSER']
 
 Before do
+  browser = Watir::Browser.new ENV['SESSION_BROWSER']
   @browser = browser
 end
 
 After do
   @browser.link(title: 'Logout').click if
     @browser.link(title: 'Logout').present?
+  @browser.close
 end
 
 at_exit do
