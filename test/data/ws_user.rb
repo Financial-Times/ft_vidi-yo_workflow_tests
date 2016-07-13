12a85abf25558c611468273fceea52bbaa8d1980
Darren Bown <dazzlebown@gmail.com>
2016-07-13 11:57:21 +0100

    Added debugging

--- test/data/ws_user.rb
+++ test/data/ws_user.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 require 'yaml'
-require_relative '../../config/config'
 
 class WSUser
-
-  include Config::Constants
 
   attr_accessor :username, :password
 
   def initialize(uname=nil, pwd=nil)
     @username = uname.nil? ? ENV['MIO_WS_USERNAME'] : uname
-    @password = pwd.nil? ? ENV['MIO_WS_PASSWORD'] : pwd
+    @password = pwd.nil? ?   ENV['MIO_WS_PASSWORD'] : pwd
+    puts "U: #{@username[1, 4]}"
+    puts "P: #{@password[1, 4]}"
   end
 
 end
