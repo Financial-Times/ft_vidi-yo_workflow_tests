e26208f3e856e678339a3b3014ad5f9d6701a47c
Darren Bown <dazzlebown@gmail.com>
2016-07-18 15:53:35 +0100

    Refactored Spec WS calls, refreshed cassettes

--- spec/project_workflow_spec.rb
+++ spec/project_workflow_spec.rb
@@ -9,30 +9,29 @@ RSpec.describe ProjectWorkflow do
 
   describe 'create_project_workflow_spec', :vcr do
     before :all do
-      @project = ProjectWorkflow.new
-    end
-
-    before :each do
-      @project = @project.create
+      @project_workflow_client = ProjectWorkflow.new
     end
 
     it 'can create a project', :vcr do
-      expect(@project.created?).to be_truthy
+      project = @project_workflow_client.create
+      expect(project.created?).to be_truthy
     end
 
     it 'can retrieve a previously-defined project', :vcr do
-      @project = @project.create
-      retrieved_project = @project.retrieve @project.id
-      expect(retrieved_project.id).to match @project.id
+      project = @project_workflow_client.create
+      retrieved_project = project.retrieve project.id
+      expect(retrieved_project.id).to match project.id
     end
 
     it 'can indicate that the workflow is complete', :vcr, wait: {timeout: 120} do
-      retrieved_project = @project.retrieve @project.id
-      wait_for_complete @project, retrieved_project
+      project = @project_workflow_client.create
+      retrieved_project = project.retrieve project.id
+      wait_for_complete project, retrieved_project
     end
 
     it 'has a uuid', :vcr do
-      expect(@project.uuid.contains_uuid?).to be_truthy
+      project = @project_workflow_client.create
+      expect(project.uuid.contains_uuid?).to be_truthy
     end
   end
 end
