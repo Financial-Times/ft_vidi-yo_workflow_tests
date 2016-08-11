ce9ad60ffaf55341c11f322667e8e1bf95499345
Darren Bown <dazzlebown@gmail.com>
2016-08-11 14:16:11 +0100

    Updated .gitignore to exclude videos

--- scripts/log_in.rb
+++ scripts/log_in.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 load('initialise.rb')
 
-USERNAME = 'masteruser'
-PASSWORD = 'masteruser'
-
 @browser.text_field(id: 'j_username').set USERNAME
 @browser.text_field(id: 'j_password').set PASSWORD
 @browser.button(id: 'submit_control-button').click
